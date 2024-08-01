60799af14906196b77c53b61753c8a16b07abba5
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2024-08-01 20:29:59 -0300

    refatoration process

--- firststeps/the-api-project1/src/main/java/com/example/demo/MathController.java
+++ firststeps/the-api-project1/src/main/java/com/example/demo/MathController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.exceptions.UnsupportedMathOperationExcepetion;
 
+import converter.NumberConverter;
+
 @RestController
 public class MathController {
 	
@@ -18,62 +20,50 @@ public class MathController {
 			method= RequestMethod.GET)
 	public Double sum(@PathVariable(value = "numberOne") String numberOne,
 					  @PathVariable(value = "numberTwo") String numberTwo	) throws Exception{
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationExcepetion("Please set a numeric value!");
 		}
-		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+		return (NumberConverter.convertToDouble(numberOne) + NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	@RequestMapping(value = "/times/{numberOne}/{numberTwo}",
 			method= RequestMethod.GET)
 	public Double times(@PathVariable(value = "numberOne") String numberOne,
 					  @PathVariable(value = "numberTwo") String numberTwo	) throws Exception{
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationExcepetion("Please set a numeric value!");
 		}
-		return convertToDouble(numberOne) * convertToDouble(numberTwo);
+		return (NumberConverter.convertToDouble(numberOne) * NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	@RequestMapping(value = "/minus/{numberOne}/{numberTwo}",
 			method= RequestMethod.GET)
 	public Double minus(@PathVariable(value = "numberOne") String numberOne,
 			@PathVariable(value = "numberTwo") String numberTwo	) throws Exception{
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationExcepetion("Please set a numeric value!");
 		}
-		return convertToDouble(numberOne) - convertToDouble(numberTwo);
+		return (NumberConverter.convertToDouble(numberOne) - NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	@RequestMapping(value = "/divided/{numberOne}/{numberTwo}",
 			method= RequestMethod.GET)
 	public Double divided(@PathVariable(value = "numberOne") String numberOne,
 			@PathVariable(value = "numberTwo") String numberTwo	) throws Exception{
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationExcepetion("Please set a numeric value!");
 		}
-		return convertToDouble(numberOne) / convertToDouble(numberTwo);
+		return (NumberConverter.convertToDouble(numberOne) / NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	@RequestMapping(value= "/sqr/{number}", method= RequestMethod.GET)
 	public Double square(@PathVariable(value= "number") String number
 			) throws Exception{
-		if(!isNumeric(number)) {
+		if(!NumberConverter.isNumeric(number)) {
 			throw new UnsupportedMathOperationExcepetion("Please set a numeric value!");
 		}
-		return Math.sqrt(convertToDouble(number));
+		return Math.sqrt(NumberConverter.convertToDouble(number));
 		}
 
-	private Double convertToDouble(String strNumber) {
-		if(strNumber == null) return 0D;
-		String number = strNumber.replaceAll(",", ".");
-		if(isNumeric(number)) return Double.parseDouble(number);
-		return null;
-	}
-
-	private boolean isNumeric(String strNumber) {
-		if(strNumber == null) return false;
-		String number = strNumber.replaceAll(",", ".");
-		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-	}
 	
 }
---
+++ firststeps/the-api-project1/src/main/java/com/example/demo/math/SimpleMath.java
@@ -0,0 +1,40 @@
+package com.example.demo.math;
+
+/*import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.exceptions.UnsupportedMathOperationExcepetion;
+
+import converter.NumberConverter;*/
+
+public class SimpleMath {
+	
+	public Double sum(Double numberOne, Double numberTwo	) {
+		
+		return (numberOne) + (numberTwo);
+	}
+	
+	
+	public Double times(Double numberOne, Double numberTwo	) {
+		return (numberOne) * (numberTwo);
+	}
+	
+	
+	public Double minus(Double numberOne, Double numberTwo	) {
+		
+		return (numberOne) - (numberTwo);
+	}
+	
+	
+	public Double divided(Double numberOne, Double numberTwo	) {
+		
+		return (numberOne) / (numberTwo);
+	}
+	
+	
+	public Double square( Double number) {
+		
+		return Math.sqrt((number));
+		}
+}
