09c07fd87eb64a7f9f71c7c44d9b1f99f68308db
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2024-09-24 21:07:02 -0300

    database yml

--- firststeps/the-api-project1/src/main/java/com/example/demo/Services/PersonService.java
+++ firststeps/the-api-project1/src/main/java/com/example/demo/Services/PersonService.java
@@ -46,6 +46,27 @@ public class PersonService {
 	}
 	
 	
+	public Person create(Person person) {
+		
+		logger.info("Creating one person!");
+		
+		return person;
+	}
+	
+	public Person update(Person person) {
+		
+		logger.info("Updating one person!");
+		
+		return person;
+	}
+
+	
+	public void delete(String id) {
+		
+		logger.info("Deleting one person!");
+		
+	}
+	
 	private Person mockPeople(int i) {
 		
 		Person person = new Person();
